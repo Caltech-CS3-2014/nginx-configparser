92ec2d8368b1e74f600db2a068fb205edb006ef5
Xintong Lin <xlin@caltech.edu>
2015-04-16 01:01:28 -0700

    edited unit tests with suggestions from teammate

--- config_parser_test.cc
+++ config_parser_test.cc
@@ -4,14 +4,14 @@
 #include <string>
 
 class NginxStringConfigTest : public ::testing::Test {
-	protected:
-		bool ParseString(const std::string& config_string) {
-			std::stringstream config_stream(config_string);
-			return parser_.Parse(&config_stream, &config_);
-		}
+    protected:
+        bool ParseString(const std::string& config_string) {
+            std::stringstream config_stream(config_string);
+            return parser_.Parse(&config_stream, &config_);
+        }
 
-		NginxConfigParser parser_;
-		NginxConfig config_;
+        NginxConfigParser parser_;
+        NginxConfig config_;
 };
 
 TEST(NginxConfigParserTest, SimpleConfigFromFile) {
@@ -24,34 +24,25 @@ TEST(NginxConfigParserTest, SimpleConfigFromFile) {
 }
 
 TEST(NginxConfigStatementTest, ToString) {
-	NginxConfigStatement statement;
-	statement.tokens_.push_back("foo");
-	statement.tokens_.push_back("bar");
+    NginxConfigStatement statement;
+    statement.tokens_.push_back("foo");
+    statement.tokens_.push_back("bar");
 
-	EXPECT_EQ("foo bar;\n", statement.ToString(0));
-}
-
-TEST(NginxConfigParserTest, SimpleTextConfig) {
-	const std::string config_test = "foo bar;";
-	std::stringstream config_stream(config_test);
-	NginxConfigParser parser;
-	NginxConfig config;
-
-	EXPECT_TRUE(parser.Parse(&config_stream, &config));
+    EXPECT_EQ("foo bar;\n", statement.ToString(0));
 }
 
 TEST_F(NginxStringConfigTest, SimpleTextConfig) {
-	EXPECT_TRUE(ParseString("foo bar;"));
+    EXPECT_TRUE(ParseString("foo bar;"));
 }
 
 TEST_F(NginxStringConfigTest, SimpleBadTextConfig) {
-	EXPECT_FALSE(ParseString("foo bar"));
+    EXPECT_FALSE(ParseString("foo bar"));
 }
 
 TEST_F(NginxStringConfigTest, SimpleNestedConfig) {
-	EXPECT_TRUE(ParseString("foo { foo bar; }"));
+    EXPECT_TRUE(ParseString("foo { foo bar; }"));
 }
 
 TEST_F(NginxStringConfigTest, DoubleNestedConfig) {
-	EXPECT_TRUE(ParseString("foo { foo { foo bar; } }"));
+    EXPECT_TRUE(ParseString("foo { foo { foo bar; } }"));
 }
